d271bcbb9063c64dba6e87111976bac7ce06d997
Max Grover <mgroverwx@gmail.com>
2021-11-16 12:07:42 -0700

    add new tasks and flows

--- cesm_collections/tasks.py
+++ cesm_collections/tasks.py
@@ -1,12 +1,14 @@
+import os
 from prefect import task
 import intake
 import calc
+import xcollection
 
 
 # Deal with Intake-ESM catalogs
 @task
 def read_catalog(path, csv_kwargs):
-    return intake.open_esm_datastore(path, read_csv_kwargs=csv_kwargs)
+    return intake.open_esm_datastore(path, csv_kwargs=csv_kwargs)
 
 @task
 def subset_catalog(catalog, search_dict):
@@ -16,6 +18,26 @@ def subset_catalog(catalog, search_dict):
 @task
 def center_time(ds, model):
     return calc.center_time(ds)
+
+@task
+def subset_dates(catalog, date_subset):
+    return catalog.search(date=catalog.df.date[date_subset].values)
+
+@task
+def convert_to_collection(dsets):
+    return xcollection.Collection(dsets)
+
+@task
+def load_data(catalog, cdf_kwargs):
+    return catalog.to_dataset_dict(cdf_kwargs=cdf_kwargs)
+
+@task
+def long_term_mean(collection):
+    return collection.map(calc.temporal_average)
+
+@task
+def annual_mean(collection):
+    return collection.map(calc.yearly_average)
 
 @task
 def global_average(ds, horizontal_dims, area_field, land_sea_mask, time_dim, include_ms=False):
@@ -28,3 +50,19 @@ def global_integral(ds, horizontal_dims, area_field, land_sea_mask, time_dim, in
 @task
 def zonal_average(da, grid, lat_field, ydim, xdim, lat_axis, region_mask=None):
     return calc.zonal_mean(da=da, grid=grid, lat_field=lat_field, ydim=ydim, xdim=xdim, lat_axis=lat_axis, region_mask=region_mask)
+
+@task
+def cache_result(collection, operation, cache_dir, cache_format='zarr'):
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+        
+    for key in collection.keys():
+        cache_path = f'{cache_dir}/{operation}/{key}.{operation}.{cache_format}'
+        dso = collection[key]
+        dso['member_id'] = key
+        dso = dso.expand_dims('member_id')
+        if cache_format == 'zarr':
+            collection[key].to_zarr(cache_path, mode='w')
+            
+        elif cache_format == 'netcdf':
+            collection[key].to_netcdf(cache_path, mode='w')

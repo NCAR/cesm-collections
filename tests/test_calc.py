9bc4368b21cda55bb8844682bd22e36884375f60
Max Grover <mgroverwx@gmail.com>
2021-11-18 15:00:43 -0700

    add data dir

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -8,8 +8,8 @@ sample_data_dir = pathlib.Path(os.path.dirname(__file__)).parent / 'data'
 
 @pytest.mark.parametrize(
     'data',
-    [sample_data_dir / 'silver_linings' / 'b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-LOWER-0.5.001.cam.h0.2035-01.nc',
-     sample_data_dir / 'mom_pop' / 'pop' / 'pop_no_mcog.pop.h.0001-01.nc' 
+    [sample_data_dir / 'cam' / 'b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-LOWER-0.5.001.cam.h0.2035-01.nc',
+     sample_data_dir / 'pop' / 'pop_no_mcog.pop.h.0001-01.nc' 
     ],
 )
 def test_center_time(data):

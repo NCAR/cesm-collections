daf080975e4fc4c220e0d9daa999dc9dd085f9da
Max Grover <mgroverwx@gmail.com>
2022-01-20 15:43:33 -0700

    add new class-based flows with tests

---
+++ tests/test_core.py
@@ -0,0 +1,15 @@
+import xarray as xr
+import xcollection as xc
+from esm_collections.core import convert_to_collection, workflow_baseclass
+
+ds = xr.tutorial.open_dataset('air_temperature')
+dsets = [ds.isel(time=0),
+         ds.isel(time=1)]
+keys = ['foo', 'bar']
+
+def test_convert_to_collection(keys=keys, dsets=dsets):
+    collection = convert_to_collection(keys=keys,
+                                       dsets=dsets)
+
+    assert isinstance(collection, xc.Collection)
+    assert isinstance(collection['foo'], xr.Dataset)
